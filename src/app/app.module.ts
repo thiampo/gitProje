5e69b0e09396b5af649b0a2c568fab756378d53c
thiampo <babacathiam@gmail.com>
2021-05-03 11:16:37 +0000

    Mise a jour Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { LoginComponent } from './login/login.component';
 import {AdminComponent}  from './admin/admin.component';
-
-
+import { ErreurSaisieComponent } from './erreur-saisie/erreur-saisie.component';
+import { EtudiantComponent } from   './etudiant/etudiant.component'
+import { FinanceComponent } from   './finance/finance.component';
+import { FormateurComponent } from   './formateur/formateur.component'
+import { IncriptionComponent } from   './incription/incription.component'
+import { MotPasseEnvoyerComponent } from './mot-passe-envoyer/mot-passe-envoyer.component';
+import {MotPasseOblierComponent} from './mot-passe-oblier/mot-passe-oblier.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +29,14 @@ import {AdminComponent}  from './admin/admin.component';
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
+    AdminComponent,
+    ErreurSaisieComponent,
+    EtudiantComponent,
+    FinanceComponent,
+    FormateurComponent,
+    IncriptionComponent,
+    MotPasseEnvoyerComponent,
+    MotPasseOblierComponent
 
 
   ],
