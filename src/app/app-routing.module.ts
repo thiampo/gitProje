bc55245a79ceb68ae8ed3417505978a83a6cf9bf
MamadouSembene <mouxammadou@gmail.com>
2021-05-15 21:02:05 +0000

    mise a jour

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { FinanceComponent } from './finance/finance.component';
+import { FormateurComponent } from './formateur/formateur.component';
+import { AdministrateurComponent } from './administrateur/administrateur.component';
+import { FormulaireInscriptionComponent } from './formulaire-inscription/formulaire-inscription.component';
+import { FormulaireModificationComponent } from './formulaire-modification/formulaire-modification.component';
+import { MotPasseOublierComponent } from './mot-passe-oublier/mot-passe-oublier.component';
 
 const routes: Routes = [
-  {path : 'login',component: LoginComponent}
+  {path : 'login',component: LoginComponent},
+  {path : 'etudiant',component: EtudiantComponent},
+  {path : 'formateur',component: FormateurComponent},
+  {path : 'finance',component: FinanceComponent},
+  {path : 'administrateur',component: AdministrateurComponent},
+  {path : 'formulaire-inscription',component: FormulaireInscriptionComponent},
+  {path : 'formulaire-modification',component:   FormulaireModificationComponent},
+  {path : 'mot-passe-oublier',component: MotPasseOublierComponent},
+  
+  
 ];
 
 @NgModule({
---
+++ src/app/formulaire-modification/formulaire-modification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-modification',
+  templateUrl: './formulaire-modification.component.html',
+  styleUrls: ['./formulaire-modification.component.css']
+})
+export class FormulaireModificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
